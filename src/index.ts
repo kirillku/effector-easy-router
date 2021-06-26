d0dcd717365364cd31aca10877ff3b0aa0d71f4e
Kirill Kubryakov <kubryakov@gmail.com>
2021-06-26 13:58:49 +0200

    Add missing CurrentRoute export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { createRoute } from "./createRoute";
-export type { Route, RouteProps } from "./createRoute";
+export type { Route, RouteProps, CurrentRoute } from "./createRoute";
 export { $location, $pathname, $search, $hash } from "./history";
 export { Link } from "./Link";
 export type { LinkProps } from "./Link";
