f04c0b6e6acdd365180a193a1bbf2aa34f569ed8
Kirill Kubryakov <kubryakov@gmail.com>
2021-06-29 15:27:23 +0200

    Fix ts types, update typescript

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import alias from "@rollup/plugin-alias";
 import strip from "@rollup/plugin-strip";
+import dts from "rollup-plugin-dts";
 
 import babel from "rollup-plugin-babel";
 import typescript from "rollup-plugin-typescript2";
@@ -15,38 +16,47 @@ const extensions = [".mjs", ".js", ".ts", ".tsx", ".json"];
 
 const isProduction = process.env.NODE_ENV === "production";
 
-export default {
-  input: "src",
-  output: [
-    {
-      file: "dist/common.js",
-      format: "cjs",
-      sourcemap: true,
-    },
-    {
-      file: "dist/index.js",
-      format: "esm",
-      sourcemap: true,
-    },
-  ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-  ],
-  plugins: [
-    alias({
-      entries: { "~": "./src" },
-      resolve: extensions,
-    }),
-    eslint(),
-    typescript({ rollupCommonJSResolveHack: true, clean: true }),
-    babel({
-      extensions,
-      exclude: "node_modules/**",
-    }),
-    resolve({ extensions }),
-    isProduction && strip(),
-    commonjs(),
-    isProduction && terser(),
-  ],
-};
+const config = [
+  {
+    input: "src",
+    output: [
+      {
+        file: "dist/common.js",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: "dist/index.js",
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ],
+    plugins: [
+      alias({
+        entries: { "~": "./src" },
+        resolve: extensions,
+      }),
+      eslint(),
+      typescript({ rollupCommonJSResolveHack: true, clean: true }),
+      babel({
+        extensions,
+        exclude: "node_modules/**",
+      }),
+      resolve({ extensions }),
+      isProduction && strip(),
+      commonjs(),
+      isProduction && terser(),
+    ],
+  },
+  {
+    input: "./dist/src/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
+
+export default config;
